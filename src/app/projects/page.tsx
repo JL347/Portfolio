b40bf173a1e95129966c1181724651d7c1f22e3a
JL347 <jaredlemkedev@gmail.com>
2024-02-14 13:16:04 -0800

    added to projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,46 +4,22 @@ import Image from 'next/image'
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
 import logoAnimaginary from '@/images/logos/animaginary.svg'
-import logoCosmos from '@/images/logos/cosmos.svg'
-import logoHelioStream from '@/images/logos/helio-stream.svg'
-import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
+import logoRequis from '@/images/logos/requis_logo_no_wordmark.png'
 
 const projects = [
   {
-    name: 'Planetaria',
+    name: 'Requis',
     description:
-      'Creating technology to empower civilians to explore space on their own terms.',
-    link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
-    logo: logoPlanetaria,
+      'Host your entire supply chain on one easy‑to‑use platform.',
+    link: { href: 'https://requis.com/', label: 'requis.com' },
+    logo: logoRequis,
   },
   {
-    name: 'Animaginary',
+    name: 'Elite Fantasy',
     description:
-      'High performance web animation library, hand-written in optimized WASM.',
+      'The all-in-one fantasy sports app - for the elite.',
     link: { href: '#', label: 'github.com' },
     logo: logoAnimaginary,
-  },
-  {
-    name: 'HelioStream',
-    description:
-      'Real-time video streaming library, optimized for interstellar transmission.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoHelioStream,
-  },
-  {
-    name: 'cosmOS',
-    description:
-      'The operating system that powers our Planetaria space shuttles.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoCosmos,
-  },
-  {
-    name: 'OpenShuttle',
-    description:
-      'The schematics for the first rocket I designed that successfully made it to orbit.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoOpenShuttle,
   },
 ]
 
@@ -60,14 +36,16 @@ function LinkIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 
 export const metadata: Metadata = {
   title: 'Projects',
-  description: 'Things I’ve made trying to put my dent in the universe.',
+  description: 'Things I’ve been working on.',
 }
 
 export default function Projects() {
   return (
     <SimpleLayout
-      title="Things I’ve made trying to put my dent in the universe."
-      intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+      title="Things I’ve been working on."
+      intro="While my current work consumes most of my time, 
+      I still find time to work on a couple of side projects. 
+      Here are a few of them."
     >
       <ul
         role="list"
